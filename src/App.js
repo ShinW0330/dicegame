b1f1c0e4b617d08a220d7816e08316247ab16be7
matai <shinwook0330@gmail.com>
2023-05-31 20:00:19 +0900

    가위 바위 보 게임 승부기록 기능 구현

--- src/App.js
+++ src/App.js
@@ -12,36 +12,41 @@ function getResult(me, other) {
 }
 
 
+
 function App() {
   const [hand, setHand] = useState("rock");
   const [otherHand, setOtherHand] = useState("rock");
-  
+  const [gameHistory , setGameHistory] = useState([]);
   
 
   const handleButtonClick = (nextHand) => {
     const nextOtherHand = generateRandomHand();
+    const nextHistoryItem = getResult(nextHand, nextOtherHand);
     setHand(nextHand);
     setOtherHand(nextOtherHand);
+    setGameHistory([...gameHistory, nextHistoryItem]);
+  
+    console.log(gameHistory);
   };
   
   
   const handleClearClick = () => {
     setHand('rock');
     setOtherHand('rock');
+    setGameHistory([]);
   };
   
   
   return (
 
 
-    <div>
-      <Button onClick={handleClearClick}>처음부터</Button>
-      <p>{getResult(hand, otherHand)}</p>
+      <div>
+        <Button onClick={handleClearClick}>처음부터</Button>
       <div>
         <HandIcon value={hand}></HandIcon>
         VS
         <HandIcon value={otherHand}></HandIcon>
-
+        <p>승부 기록 : {gameHistory.join(',')}</p>
       </div>
       
       <HandButton value="rock" onClick={handleButtonClick} />
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import DiceApp from './DiceApp';
 
-//ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<DiceApp />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<DiceApp />, document.getElementById('root'));
