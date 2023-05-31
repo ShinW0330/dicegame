40d22b1dfa31f56b04f7dff36a7a0422659e5f10
matai <shinwook0330@gmail.com>
2023-05-31 15:30:45 +0900

    diceGame 점수 기능과 기록 기능 추가.

--- src/DiceApp.js
+++ src/DiceApp.js
@@ -9,21 +9,39 @@ function random(n){
 function DiceApp() {
   // 숫자 state 설정
   const [num, setNum] = useState(1);
+  const [sum, setSum] = useState(0);
+  const [gameHistory, setGameHistory] = useState([]);
+  //const [dot] = useState("");
   //const [reset, setReset] = useState(1);
 
   const handleRollClick = () => {
     const nextNum = random(6);
     setNum(nextNum);
+    setSum(sum + nextNum);
+    //console.log(sum);
+    const gameHistoryCopy = [...gameHistory];
+    gameHistoryCopy.push(nextNum);
+    setGameHistory(gameHistoryCopy)
   };
   const handleClearClick = () => {
-    setNum(1);
+    setNum(1); 
+    setSum(0); 
+    setGameHistory([]);
+
   }
 
   return (
     <div>
-      <Button onClick ={handleRollClick}>던지기</Button>
-      <Button onClick ={handleClearClick}>처음부터</Button>
-      <Dice color="red" num={num} />
+      <div>
+        <Button onClick ={handleRollClick}>던지기</Button>
+        <Button onClick ={handleClearClick}>처음부터</Button>
+      </div>
+        <h2>나</h2>
+      <Dice color="blue" num={num}></Dice>
+      <h2>총점</h2>
+      <p>{sum}</p>
+      <h2>기록</h2>
+      <p>{gameHistory.join(', ')}</p>
     </div>
   );
 }
